9014a2cbd27a624a945f73d874d54dff103e1d25
3a632076 <moon9638150@gmail.com>
2019-12-04 11:57:52 +0800

    設定HomeController，且將路由設定好

--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+// 認證路由...
+Route::auth();
 
 Route::get('/', function () {
     return view('welcome');
 });
-// 認證路由...
-Route::auth();
+Route::get('/home', function () {
+    return view('home.home');
+});
+Route::get('/tasks', 'TaskController@index');
+Route::post('/task', 'TaskController@store');
+Route::delete('/task/{task}', 'TaskController@destroy');

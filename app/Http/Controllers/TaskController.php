a96c0c902ef8be1748a30467eda1675015534806
3A632076 <jg9638150@gmail.com>
2019-12-18 11:52:56 +0800

    驗證收到的表單

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -19,4 +19,12 @@ class TaskController extends Controller
     {
         return view('tasks.index');
     }
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        // Create The Task...
+    }
 }

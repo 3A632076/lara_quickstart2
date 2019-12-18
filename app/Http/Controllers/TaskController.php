012816aaffb7894c3aacfb5ace0002d767d4c929
3A632076 <jg9638150@gmail.com>
2019-12-18 11:55:47 +0800

    顯示已有任務

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -17,7 +17,14 @@ class TaskController extends Controller
     }
     public function index(Request $request)
     {
-        return view('tasks.index');
+        public function index(Request $request)
+    {
+        $tasks = Task::where('user_id', $request->user()->id)->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
     }
 
     public function store(Request $request)

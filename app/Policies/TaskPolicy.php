5b5af65b8e223c1e79cc107171e06d289ae13156
3A632076 <jg9638150@gmail.com>
2019-12-25 11:23:42 +0800

    增加一個 destroy 方法到 policy

--- app/Policies/TaskPolicy.php
+++ app/Policies/TaskPolicy.php
@@ -3,6 +3,7 @@
 namespace App\Policies;
 
 use App\User;
+use App\Task;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 class TaskPolicy
@@ -17,5 +18,16 @@ class TaskPolicy
     public function __construct()
     {
         //
+    }
+    /**
+     * 判斷當給定的使用者可以刪除給定的任務。
+     *
+     * @param  User  $user
+     * @param  Task  $task
+     * @return bool
+     */
+    public function destroy(User $user, Task $task)
+    {
+        return $user->id === $task->user_id;
     }
 }
